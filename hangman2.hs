a5447898ae20d200250c4ae6b4b8911a5e5b74ed
Rikkokiri <pilvi.rajala@gmail.com>
2017-06-04 17:27:49 +0300

    Moving stuff to functions and variables

--- hangman2.hs
+++ hangman2.hs
@@ -9,6 +9,9 @@ import Control.Monad
 goalword :: String
 goalword = "hangman"
 
+-- | Rules of the game
+rules :: String
+rules = "    H A N G M A N   G A M E \n ===============================\n I'm thinking of a word and you must guess what it is. You may guess either letters or if you're daring, the entire word.\n If you guess wrong, you'll get one step closer to death. The ninth failure will kill you. Good luck!\n"
 
 -- | 
 hideWord :: String -> String
@@ -49,9 +52,9 @@ maxGuesses :: Int
 maxGuesses = length drawing
 
 
--- | Print the message of how many guesses are left
+-- | Print the message of how many guesses are left (TODO FIX! The message is not guessses left, but failures)
 guessesLeftMsg :: Int -> String
-guessesLeftMsg guessNumber = ("\nYou have " ++ (show (maxGuesses - (succ guessNumber))) ++ " guesses left\n")
+guessesLeftMsg guessNumber = ("You have " ++ (show (maxGuesses - (succ guessNumber))) ++ " wrong guesses left")
 
 
 -- | Guessing. If the length of @param guess == 1, then 
@@ -65,39 +68,38 @@ takeAGuess guess word
 guessWord :: String -> String
 guessWord guess
     | guess == goalword = goalword
-    | otherwise = "Unfortunately your guess was wrong"
+    | otherwise = "Unfortunately your guess " ++ guess ++ " was wrong"
 
 
 -- | Check if a given character is in the string
--- | @param guess, goal word, partly hidden word
+-- | @param guess
+-- | @param (x:xs)
+-- | @param (y:ys) TODO
 guessChar :: Char -> String -> String -> String
 guessChar guess [] [] = ""
 guessChar guess (x:xs) (y:ys)
-    -- Letters match
     | guess == x = (charToString x) ++ guessChar guess xs ys
     | elem y ['a'..'z'] = (charToString y) ++ guessChar guess xs ys
     | otherwise = "*" ++ guessChar guess xs ys
 
 
--- | 
+-- | Convert Char to String
 charToString :: Char -> String
 charToString x = [x]
 
 
--- |
+-- | Print a list of Chars separated with commas
 printCharArray :: [Char] -> String
 printCharArray [] = ""
 printCharArray [x] = (charToString x)
 printCharArray (x:xs) = (charToString x) ++ ", " ++ (printCharArray xs)
 
 
---| Start the game
+-- | Start the game
 hangman = do
+
     -- TODO Tell player the rules of the game
-    putStrLn "    H A N G M A N   G A M E"
-    putStrLn "==============================="
-    putStrLn "I'm thinking of a word and you must guess what it is. You may guess either letters or if you're daring, the entire word."
-    putStrLn "If you guess wrong, you'll get one step closer to death. The ninth failure will kill you. Good luck!"
+    putStrLn rules
 
     -- Cover the letters in the goal word
     let toBeGuessed = (hideWord goalword)
@@ -108,6 +110,14 @@ hangman = do
     -- Move to guessing the word
     gameplay 0 toBeGuessed guessedLetters
 
+
+
+validateGuess :: Char -> [Char] -> String
+validateGuess guess letters
+    -- Check that guessed char was a letter
+
+    -- Check if the given guess has already been guessed
+    | (elem guess letters) = "You have already guessed that! Guess again!"
 
 
 -- | Run the actual gameplay
@@ -121,13 +131,9 @@ gameplay guessNumber word letters = do
     putStr "Guess a letter or the entire word: "
     guess <- getLine
 
-
-    -- Check if guess was a character and if that character has already been guessed
-    -- if so, prompt the player to guess again
-    if (length guess == 1) && (elem (head guess) letters) then do
-        putStrLn "You have already guessed that! Guess again!"
+    -- Check if 
+    if (validateGuess (head guess) letters) /= "" then do
         gameplay guessNumber word letters
-
         
     -- If the guess is new, hangle the guess
     else do
@@ -154,7 +160,7 @@ gameplay guessNumber word letters = do
 
             -- If there are guesses left, continue the game
             else do
-                putStrLn guessesLeftMsg
+                putStrLn (guessesLeftMsg guessNumber)
 
                 if(length guess == 1) then
                     gameplay (succ guessNumber) result (letters ++ guess)
